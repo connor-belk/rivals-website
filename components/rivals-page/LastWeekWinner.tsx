3d4d1c7da6b19ea972377788d700a2a1583e0128
Connor Belk <connor.belk@gmail.com>
2024-05-13 21:14:18 -0400

    fixed apostrophe in LastWeekWinner.tsx

--- components/rivals-page/LastWeekWinner.tsx
+++ components/rivals-page/LastWeekWinner.tsx
@@ -2,7 +2,7 @@ export default function LastWeekWinner() {
   return (
     <div className="text-slate-200 h-[15vh] md:h-[40vh] m-2 bg-slate-500 rounded-3xl flex flex-col items-center justify-center">
       <h3 className="ml-2 mb-2 text-3xl font-bold md:text-5xl text-center">
-        Last Week's Winner
+        Last Week&apos;s Winner
       </h3>
       <ul className="flex flex-col justify-center items-center">
         <li>profile pic</li>
