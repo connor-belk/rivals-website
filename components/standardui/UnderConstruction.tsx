34d582d906258ad332b19c14c78b3796071ba347
Connor Belk <connor.belk@gmail.com>
2024-05-16 15:10:15 -0400

    fixed apos in UnderConstruction module.

--- components/standardui/UnderConstruction.tsx
+++ components/standardui/UnderConstruction.tsx
@@ -5,8 +5,8 @@ export default function UnderContruction({ pageName }: { pageName?: string }) {
     <div className="text-white h-[600px] w-[80%] md:w-[50%] mx-auto flex flex-col items-center justify-center gap-7">
       <h2 className="text-4xl">{pageName}</h2>
       <p className="mx-auto text-center">
-        We're Sorry. The {pageName} page is currently under construction. Please
-        check back later!
+        We&apos;re Sorry. The {pageName} page is currently under construction.
+        Please check back later!
       </p>
       <Link href="/" className="underline text-sm">
         Click here to go back to the home page
