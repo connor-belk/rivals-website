e6239e2953bd03b79eb7341b4d5aadcd2a186c7d
Connor Belk <connor.belk@gmail.com>
2024-05-17 21:13:41 -0400

    temporary fix for wheel-to-wheel UI appearance, removed podium from smaller screens

--- app/wheel-to-wheel/page.tsx
+++ app/wheel-to-wheel/page.tsx
@@ -6,28 +6,40 @@ import React from "react";
 export default function WheelToWheelPage() {
   return (
     <div>
-      <h1 className="hidden md:block text-5xl text-slate-200 font-extrabold tracking-wider text-center mt-2">
+      <h1
+        id="desktop-heading"
+        className="hidden md:block text-5xl text-slate-200 font-extrabold tracking-wider text-center mt-2"
+      >
         Wheel to Wheel
       </h1>
-      <h1 className="md:hidden text-4xl text-slate-200 font-extrabold tracking-wider text-center mt-2">
+
+      <h1
+        id="mobile-heading"
+        className="md:hidden text-4xl text-slate-200 font-extrabold tracking-wider text-center mt-2"
+      >
         Wheel to Wheel
       </h1>
+
       <div className="w-full flex flex-col justify-center items-center mt-4 mb-8">
         <div className="border border-slate-300/40 w-[90%]"></div>
       </div>
-      <div className="md:grid grid-cols-3 grid-rows-1">
+
+      <div className="lg:grid lg:grid-cols-3 lg:grid-rows-1">
         <div className="col-span-2 mb-8 md:mb-0">
           <UpcomingRace />
         </div>
 
         <LastWeekPodium />
       </div>
+
       <h2 className="text-3xl text-white font-bold tracking-wider text-center mb-2 mt-10">
         Previous Results
       </h2>
+
       <div className="w-full flex flex-col justify-center items-center">
         <div className="border border-slate-300/40 w-[90%]"></div>
       </div>
+
       <div className="mx-10 mt-10">
         <PreviousResults />
       </div>
--- components/wheel-page/LastWeekPodium.tsx
+++ components/wheel-page/LastWeekPodium.tsx
@@ -1,24 +1,24 @@
 export default function LastWeekPodium() {
   return (
-    <div className="text-slate-200 h-[20vh] md:h-[40vh] m-2 bg-slate-500 rounded-3xl flex flex-col items-center justify-start">
+    <div className="hidden text-slate-200 h-[20vh] lg:h-[40vh] min-h-48 m-2 bg-slate-500 rounded-3xl lg:flex flex-col items-center justify-start">
       <h3 className="ml-2 mb-2 mt-5 text-3xl font-bold md:text-5xl text-center">
         Last Week&apos;s Podium
       </h3>
-      <div className="grid grid-cols-3 grid-rows-1 gap-4 text-black items-center justify-center">
-        <ul className="flex flex-col justify-center items-center bg-[#FFBF00] p-2 rounded-xl">
+      <div className="grid grid-cols-3 grid-rows-1 lg:grid-cols-1 ld:grid-rows-3 gap-4 text-black items-center justify-center mt-6">
+        <ul className="grid grid-cols-5 grid-rows-1 items-center justify-center gap-3 bg-[#FFBF00] p-2 rounded-xl">
           <li className="font-bold text-xl">1st</li>
-          <li>username</li>
-          <li>time, car</li>
+          <li className="col-span-2 font-bold">NINJA goa1ie</li>
+          <li className="col-span-2 font-bold">13:34.647</li>
         </ul>
-        <ul className="flex flex-col justify-center items-center bg-[#c0c0c0] p-2 rounded-xl">
+        <ul className="grid grid-cols-5 grid-rows-1 items-center justify-center gap-3 bg-[#c0c0c0] p-2 rounded-xl">
           <li className="font-bold text-xl">2nd</li>
-          <li>username</li>
-          <li>time, car</li>
+          <li className="col-span-2 font-bold">NINJA goa1ie</li>
+          <li className="col-span-2 font-bold">13:34.647</li>
         </ul>
-        <ul className="flex flex-col justify-center items-center bg-[#CD7F32] p-2 rounded-xl">
+        <ul className="grid grid-cols-5 grid-rows-1 items-center justify-center gap-3 bg-[#CD7F32] p-2 rounded-xl">
           <li className="font-bold text-xl">3rd</li>
-          <li>username</li>
-          <li>time, car</li>
+          <li className="col-span-2 font-bold">NINJA goa1ie</li>
+          <li className="col-span-2 font-bold">13:34.647</li>
         </ul>
       </div>
     </div>
