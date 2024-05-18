b91f7ba0573996ea1386c7ed7373b9ba970c7422
Connor Belk <connor.belk@gmail.com>
2024-05-17 21:22:44 -0400

    fixed rivals page UI appearance to work correctly on smaller screens, reworked UI for last week winnder tile

--- components/rivals-page/LastWeekWinner.tsx
+++ components/rivals-page/LastWeekWinner.tsx
@@ -4,10 +4,13 @@ export default function LastWeekWinner() {
       <h3 className="ml-2 mb-2 text-3xl font-bold md:text-5xl text-center">
         Last Week&apos;s Winner
       </h3>
-      <ul className="flex flex-col justify-center items-center">
-        <li>profile pic</li>
-        <li>username</li>
-        <li>time, car, track, PI rating</li>
+      <ul className="flex md:flex-col justify-center items-center w-full gap-4 mt-4">
+        <div className="flex items-center justify-center gap-2">
+          <li className="w-10 h-10 md:w-14 md:h-14 bg-slate-600 rounded-full"></li>
+          <li className="text-2xl font-bold">NINJA goa1ie</li>
+        </div>
+        <li className="text-2xl">01:23.456</li>
+        <li className="text-xl border px-2">PI: 721</li>
       </ul>
     </div>
   );
